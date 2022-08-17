e7cb6683ceab7531b1e1c6809b8d411f52c2f7da
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2022-08-17 21:53:32 +0200

    better images

--- src/CacheRebuilder.php
+++ src/CacheRebuilder.php
@@ -24,12 +24,13 @@ class CacheRebuilder
                 'flag' => dirname(__DIR__) . '/assets/flag.png',
                 'ps_plus' => dirname(__DIR__) . '/assets/ps-plus.webp',
                 'trophy' => [
-                    'platinum' => dirname(__DIR__) . '/assets/trophy-platinum.png',
-                    'gold' => dirname(__DIR__) . '/assets/trophy-gold.png',
-                    'silver' => dirname(__DIR__) . '/assets/trophy-silver.png',
-                    'bronze' => dirname(__DIR__) . '/assets/trophy-bronze.png',
+                    'platinum' => dirname(__DIR__) . '/assets/trophies/platinum.png',
+                    'gold' => dirname(__DIR__) . '/assets/trophies/gold.png',
+                    'silver' => dirname(__DIR__) . '/assets/trophies/silver.png',
+                    'bronze' => dirname(__DIR__) . '/assets/trophies/bronze.png',
                 ],
             ],
+            // https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap
             'fonts' => [
                 [
                     'family' => 'Poppins',
@@ -43,16 +44,16 @@ class CacheRebuilder
                     'icon' => 'https://playstation-trophy-cards.robiningelbrecht.be/assets/profile/trophies/2S69db6a.png',
                     'title' => '1000 Talismans Shot',
                     'description' => 'Shoot 1,000 talismans.',
-                    'grade' => dirname(__DIR__) . '/assets/trophy-bronze.png',
-                    'rarity' => 'https://playstation-trophy-cards.robiningelbrecht.be/assets/images/rarity-sprite.png',
+                    'grade' => dirname(__DIR__) . '/assets/trophies/bronze.png',
+                    'rarity' => dirname(__DIR__) . '/assets/rarity/ultra-rare.png',
                     'earned_on' => '12/24/2021 1:17PM',
                 ],
                 [
                     'icon' => 'https://playstation-trophy-cards.robiningelbrecht.be/assets/profile/trophies/2S69db6a.png',
                     'title' => '1000 Talismans Shot',
                     'description' => 'Shoot 1,000 talismans.',
-                    'grade' => dirname(__DIR__) . '/assets/trophy-bronze.png',
-                    'rarity' => 'https://playstation-trophy-cards.robiningelbrecht.be/assets/images/rarity-sprite.png',
+                    'grade' => dirname(__DIR__) . '/assets/trophies/bronze.png',
+                    'rarity' => dirname(__DIR__) . '/assets/rarity/rare.png',
                     'earned_on' => '12/24/2021 1:17PM',
                 ],
             ],
